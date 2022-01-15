b63ef15f09a814bc8eeab4f446b55f6252954732
harshawnsingh <hsingh56@ucsc.edu>
2022-01-15 13:47:11 -0800

    adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+//import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDrsxdyCfNuYHLveYFnBMVcG8eX_mEkv5Y",
+  authDomain: "education-app-e4d66.firebaseapp.com",
+  projectId: "education-app-e4d66",
+  storageBucket: "education-app-e4d66.appspot.com",
+  messagingSenderId: "1097255251729",
+  appId: "1:1097255251729:web:07b13d04401c4a647526b0",
+  measurementId: "G-K63S24L3S5"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
