6049ae59428b2ea2248330c648c3a75e4272d66a
Anish Sownderraj <anishsownderraj@Anishs-MBP.attlocal.net>
2022-01-16 06:16:06 -0800

    "Added Final Final touches"

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,7 @@ class NavBar extends Component {
                         <h1 style={{fontSize:'25px',color:'black',border: '5px solid black',letterSpacing: '5px',padding: '2px'}}>
                         Specialized Success<i style={{fontSize:'10px'}}>©</i>
                         </h1>
-                        <a style={{fontFamily:'Fantasy',fontSize:'25px',color:'black',position:"absolute",top:'15px',left:'1340px'}} className="nav-item nav-link " href="#">About</a>
+                        <a style={{fontFamily:'"Lucida Console", "Courier New", monospace',fontSize:'25px',color:'black',position:"absolute",top:'12px',left:'1328px'}} className="nav-item nav-link " href="#">About</a>
 
                     </div>
                 </div>
